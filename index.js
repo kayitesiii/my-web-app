8ab221acd0121faeff4b1d0564dc271ec781c967
Chartine <kayitesichartine6@gmail.com>
2025-11-30 08:24:14 -0800

    Initial commit: Add basic Node.js web app

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, this is a basic web app!'); // Matches expected outcome 
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
